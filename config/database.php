21b1739fc98d1de14788800d4ba1d4ec55de908e
JacquysBarbosadaSilva <jacquys0801@gmail.com>
2025-09-25 15:44:25 -0300

    nome do banco trocado

--- config/database.php
+++ config/database.php
@@ -3,13 +3,14 @@
 $servername = "database-redesocial.cv62c22guy53.us-east-1.rds.amazonaws.com"; // Seu endpoint RDS
 $username = "admin"; // Usuário master do RDS
 $password = "redeSocial!"; // Senha do RDS
-$dbname = "database_redeSocial"; // Nome do banco inicial
+$dbname = "database_redevibe"; // Nome do banco inicial
 $port = "3306";
 
 try {
     $pdo = new PDO("mysql:host=$servername;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_TIMEOUT, 10); // Timeout de 10 segundos
+    echo "<script>console.log('Conexão com RDS estabelecida com sucesso!');</script>";
 
 } catch(PDOException $e) {
     echo "Erro na conexão com RDS: " . $e->getMessage();
